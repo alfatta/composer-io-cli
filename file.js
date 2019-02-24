dad790929fc4d68975d3a5418cbe7120fe848cfc
Alfatta Rezqa <alfatta@damcorp.id>
2019-02-25 01:11:19 +0700

    List file give output string not json

--- file.js
+++ file.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 fs.readdir('./source', (err, files) => {
   if (!err) {
     let regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.png|.gif|.svg)$");
-    console.log(files.filter(file => regex.test(file.toLowerCase())));
+    console.log(files.filter(file => regex.test(file.toLowerCase())).join('\n'));
   }
 });
